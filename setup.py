34e72f033ff118cf261d776258821aef01a71b11
raffaelecheula <cheula.raffaele@gmail.com>
2021-10-25 18:26:08 +0200

    moved files in examples

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="nanoparticles_ensembles",
+    version="0.1.1",
+    url="https://github.com/raffaelecheula/nanoparticles_ensembles.git",
+
+    author="Raffaele Cheula",
+    author_email="cheula.raffaele@gmail.com",
+
+    description="General purpose tools for high-throughput catalysis.",
+    long_description=readme,
+    license='GPL-3.0',
+
+    packages=[
+        'nanoparticles',
+    ],
+    package_dir={
+        'nanoparticles': 'nanoparticles'
+    },
+    install_requires=requirements,
+    python_requires='>=3.5, <4',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+    ],
+)
