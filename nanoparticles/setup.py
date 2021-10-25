cfeb242bedaa11e05d29a0eee70926f9650a0ccb
raffaelecheula <32555922+raffaelecheula@users.noreply.github.com>
2021-10-25 14:50:05 +0200

    Update setup.py

--- nanoparticles/setup.py
+++ nanoparticles/setup.py
@@ -2,10 +2,12 @@
 # SETUP CYTHON
 ################################################################################
 
+import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize("nanoparticle_cython.pyx", annotate = True))
+setup(ext_modules  = cythonize("nanoparticle_cython.pyx", annotate = True),
+      include_dirs = [numpy.get_include()])
 
 ################################################################################
 # END
